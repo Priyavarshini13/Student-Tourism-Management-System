7308cf8e4dfd9a0b80325cee85ee75b4f0578aea
Priyavarshini13 <priyavarshinivaratharajan@gmail.com>
2026-05-07 10:43:51 +0530

    Updated production backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://student-tourism-api.onrender.com/api" });
 
 // Attach token automatically
 API.interceptors.request.use((config) => {
